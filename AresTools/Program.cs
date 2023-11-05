f85903602ae89c5c1ebd21aad83651aecabb5258
Pablo <pablo.hehe2@gmail.com>
2023-11-05 02:23:02 -0300

    Update Program.cs
    Fixed enum RoomLanguage

--- AresTools/Program.cs
+++ AresTools/Program.cs
@@ -7,11 +7,28 @@ using System.Text;
 using cb0t;
 
 
-public enum RoomLanguage
+public enum RoomLanguage : byte
 {
-    English,
-    Spanish,
-    // Add other languages as needed
+        Arabic = 11,
+        Chinese = 12,
+        Czech = 14,
+        Danish = 15,
+        Dutch = 16,
+        English = 10,
+        Finnish = 27,
+        French = 28,
+        German = 29,
+        Italian = 30,
+        Japanese = 17,
+        Kirghiz = 19,
+        Polish = 20,
+        Portuguese = 21,
+        Russian = 31,
+        Slovak = 22,
+        Spanish = 23,
+        Swedish = 25,
+        Turkish = 26,
+        Any = 255
 }
 
 public class ChannelListItem
