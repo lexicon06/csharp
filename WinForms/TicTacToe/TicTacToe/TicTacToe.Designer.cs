90d392bc024d91faf9335a881a37fc1e03b1d82c
Pablo <pablo.hehe2@gmail.com>
2023-11-10 21:08:04 -0300

    Added some logic to forms

--- WinForms/TicTacToe/TicTacToe/TicTacToe.Designer.cs
+++ WinForms/TicTacToe/TicTacToe/TicTacToe.Designer.cs
@@ -29,20 +29,34 @@ namespace TicTacToe
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "TicTacToe";
             this.Text = "TicTacToe";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
